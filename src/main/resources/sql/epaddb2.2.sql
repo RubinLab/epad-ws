be0692f3beb8a35bed42c08d66e05effb0ca11e9
Emel Alkim <emelalkim@msn.com>
2016-08-30 10:54:30 -0700

    removed drop column

--- src/main/resources/sql/epaddb2.2.sql
+++ src/main/resources/sql/epaddb2.2.sql
@@ -1,4 +1,3 @@
-ALTER TABLE epad_file DROP COLUMN templateleveltype; 
 ALTER TABLE epad_file ADD COLUMN templateleveltype varchar(10) NULL;
 UPDATE epad_file set templateleveltype = 'image' WHERE filetype = 'Template' and templateleveltype is null;
 
