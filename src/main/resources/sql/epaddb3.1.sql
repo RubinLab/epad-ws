63b122d643b694d5ab94e6bdb61d48799e9a8b41
Emel Alkim <emelalkim@msn.com>
2018-05-01 12:13:28 -0700

    updates the version

--- src/main/resources/sql/epaddb3.1.sql
+++ src/main/resources/sql/epaddb3.1.sql
@@ -2,5 +2,5 @@ create index STUDYUID_INDEX ON annotations (StudyUID);
 create index PATIENTID_INDEX ON annotations (PatientID);
 create index USER_FOREIGN_KEY_INDEX ON annotations (userloginname);
 
-UPDATE dbversion SET version = '3.0';
+UPDATE dbversion SET version = '3.1';
 commit;
