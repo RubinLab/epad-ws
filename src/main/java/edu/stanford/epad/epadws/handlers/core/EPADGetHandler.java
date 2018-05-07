d7f7a736b02852ca100c7398cac2400122a9e593
Emel Alkim <emelalkim@msn.com>
2018-05-07 13:05:51 -0700

    uses filterpermitted annotations instead of summaries

--- src/main/java/edu/stanford/epad/epadws/handlers/core/EPADGetHandler.java
+++ src/main/java/edu/stanford/epad/epadws/handlers/core/EPADGetHandler.java
@@ -843,7 +843,7 @@ public class EPADGetHandler
 				String templateCode = httpRequest.getParameter("templatecode");
 				String[] shapes = (httpRequest.getParameter("shapes")!=null)?httpRequest.getParameter("shapes").split(","):null; //comma separated values
 				if (report!=null) {
-					aims = AIMUtil.queryAIMImageAnnotationSummariesV4(aims, username, sessionID);
+					aims = AIMUtil.filterPermittedImageAnnotations(aims, username, sessionID);
 					if (report.equalsIgnoreCase("RECIST")){
 						RecistReport recistTable=AimReporter.getRecist(aims);
 						if (recistTable!=null)
@@ -874,7 +874,7 @@ public class EPADGetHandler
 						}
 					}
 				} else if (returnTable) {
-					aims = AIMUtil.queryAIMImageAnnotationSummariesV4(aims, username, sessionID);
+					aims = AIMUtil.filterPermittedImageAnnotations(aims, username, sessionID);
 					String columns = httpRequest.getParameter("columns"); //comma seperated
 					if (columns!=null) {
 						String[] columnsArray=columns.split(",");
